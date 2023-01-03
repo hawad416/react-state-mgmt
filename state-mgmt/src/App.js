c00d50ed3a1725aeab7b694ea5bc626c009bdbce
Hawa Ali <hawad416@gmail.com>
2023-01-03 02:10:21 -0800

    practice

--- state-mgmt/src/App.js
+++ state-mgmt/src/App.js
@@ -1,5 +1,47 @@
 import logo from './logo.svg';
 import {useState} from "react";
+import React from 'react';
+
+
+function NameList(){
+  const [list, setList] = useState(["hawa", "em", "zohar"]);
+  const [name, setName] = useState("");
+
+  return(
+    <div>
+      <ul>
+        {list.map((name, i) => (
+          <li key={i}>{name}</li>
+        ))}
+      </ul>
+
+      <input
+      id="tInput"
+      type="text"
+      value={name}
+      onChange={(e) =>{
+        setName(e.target.value);
+      } }>
+      </input>
+
+      <button
+      type='submit'
+      value="submit"
+      onClick={
+        () => {
+          //list.push(name); arrays and objects use reference semantics, so if you give react a reference to an array, it 
+          //holds not the array data itself but a ref to it and it gives us back the same reference 
+          // thats why if we do a refresh we get that updated data but react has no idea we did that. 
+            setName('');
+            setList([...list, name]); //this is better instead of in place mutating 
+        }
+      }
+      > Add Name to The List!
+
+      </button>
+    </div>
+  )
+}
 
 function App() {
 
@@ -7,7 +49,7 @@ function App() {
   //meaning that if we created multiple Counter componenet, they each have their own 
   // reference to the count state. The state is couple iwht the instance of that component 
   // therefore each instnace can be indpendetly mutated and will manage its state independentl/
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(0); // scalar state mgmt 
 
   function addOne(){
     setCount(count + 1); // critical to understanding all of state
@@ -21,6 +63,7 @@ function App() {
     <div className="App">
       <p>Hello!</p>
       <button onClick={addOne}>Count = {count}</button>
+      <NameList></NameList>
     </div>
   );
 }
